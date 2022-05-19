d96b697694fdf4e284410c7da14f3dbc1dc7c944
Quang <nmquang2012@gmail.com>
2022-05-19 23:42:20 +0700

    Updated: Convert daily forecast to slider

--- src/components/DayForecast.tsx
+++ src/components/DayForecast.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+import { useState } from 'react';
 import { IData } from '../global';
 
 interface IProps {
@@ -23,33 +25,55 @@ const DayForecast = ({ dataApp }: IProps) => {
         return `${day} ${date}`;
     };
 
+    const scrollPrev = () => {
+        const list = document.querySelector('.row-forecast-list');
+        if (list) {
+            list.scrollLeft -= 300;
+        }
+    };
+
+    const scrollNext = (e: any) => {
+        const list = document.querySelector('.row-forecast-list');
+        if (list) {
+            list.scrollLeft += 300;
+        }
+    };
+
     return (
         <section className="day-forecast mt-5">
-            <p className="day-forecast-caption"> 7 day forecast </p>
-            <div className="row gx-2 day-forecast-list">
-                {dailyWeatherData.map((data, index) => (
-                    <div className="col" key={index}>
-                        <div className="day-forecast-item">
-                            <div className="date">{getDate(data.dt)}</div>
-                            <div className="row gx-2">
-                                <div className="col item-img">
-                                    <img
-                                        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                                        alt=""
-                                    />
-                                </div>
-                                <div className="col day-temperature">
-                                    <span>
-                                        {Math.round(data.temp.max)}&deg;
-                                    </span>
-                                    <span>
-                                        {Math.round(data.temp.min)}&deg;
-                                    </span>
+            <p className="day-forecast-caption"> 8 day forecast </p>
+            <div className="day-forecast-list">
+                <div className="row gx-2 row-forecast-list">
+                    {dailyWeatherData.map((data, index) => (
+                        <div className="col-4 col-sm-3 col-md-2" key={index}>
+                            <div className="day-forecast-item">
+                                <div className="date">{getDate(data.dt)}</div>
+                                <div className="row gx-2">
+                                    <div className="col item-img">
+                                        <img
+                                            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                                            alt=""
+                                        />
+                                    </div>
+                                    <div className="col day-temperature">
+                                        <span>
+                                            {Math.round(data.temp.max)}&deg;
+                                        </span>
+                                        <span>
+                                            {Math.round(data.temp.min)}&deg;
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                    <button className="btn-prev" onClick={scrollPrev}>
+                        &lt;
+                    </button>
+                    <button className="btn-next" onClick={scrollNext}>
+                        &gt;
+                    </button>
+                </div>
             </div>
         </section>
     );
